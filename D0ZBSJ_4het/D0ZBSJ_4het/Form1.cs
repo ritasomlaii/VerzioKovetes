e1c13a1a8abdfa4bdab0d9199eae93ec0798444e
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-10-06 11:38:19 +0200

    exportálás3

--- D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
+++ D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
@@ -65,6 +65,17 @@ namespace D0ZBSJ_4het
             }
         }
         
+        private string SetElevator(bool elevator)
+        {
+            if (elevator)
+            {
+                return "Van";
+            }
+            else
+            {
+                return "Nincs";
+            }
+        }
 
         public void CreateTable()
         {
@@ -86,6 +97,14 @@ namespace D0ZBSJ_4het
             for (int i = 0; i < headers.Length; i++)
             {
                 xlSheet.Cells[1, 1] = headers[0];
+                xlSheet.Cells[1, 2] = headers[1];
+                xlSheet.Cells[1, 3] = headers[2];
+                xlSheet.Cells[1, 4] = headers[3];
+                xlSheet.Cells[1, 5] = headers[4];
+                xlSheet.Cells[1, 6] = headers[5];
+                xlSheet.Cells[1, 7] = headers[6];
+                xlSheet.Cells[1, 8] = headers[7];
+                xlSheet.Cells[1, 9] = headers[8];
             }
 
             object[,] values = new object[flats.Count, headers.Length];
@@ -99,18 +118,33 @@ namespace D0ZBSJ_4het
                 values[counter, index++] = item.Vendor;
                 values[counter, index++] = item.Side;
                 values[counter, index++] = item.District;
-                values[counter, index++] = item.Elevator;
+                values[counter, index++] = SetElevator(item.Elevator);
                 values[counter, index++] = item.NumberOfRooms;
                 values[counter, index++] = item.FloorArea;
                 values[counter, index++] = item.Price;
                 //values[counter, index++] = item.;
                 values[counter, index++] = "";
-                counter++;                   
+                counter++;
             }
+                for (int i = 0; i < values.GetLength(0); i++)
+                {
+                    values[i, 8] = "=" + GetCell(i + 2, 8) + "/" + GetCell(i + 2, 7);
+                }
+            
 
+            //= values[0,7] / values[0,6]
             xlSheet.get_Range(
-                        GetCell(2, 1),
-                        GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+                             GetCell(2, 1),
+                             GetCell(1 + values.GetLength(0), values.GetLength(1)))
+                .Value2 = values;
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
 
         public string GetCell(int x, int y)
