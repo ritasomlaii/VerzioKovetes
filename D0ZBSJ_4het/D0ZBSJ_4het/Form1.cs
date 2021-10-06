fb1c970b6aaf700b9e54a820ecd838884e466b8e
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-10-06 09:50:59 +0200

    Excel munkafüzet

--- D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
+++ D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
@@ -16,15 +16,59 @@ namespace D0ZBSJ_4het
     {
         List<Flat> flats;
         RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         public void LoadData()
         {
             flats = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+               // CreateTable(); 
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        
+
+        public void CreateTable()
+        {
+
         }
     }
 }
