451198b1bf8a9f2df238c37860b7218c1a234ae0
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-10-06 09:40:46 +0200

    excel behivatkozáa

--- D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
+++ D0ZBSJ_4het/D0ZBSJ_4het/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace D0ZBSJ_4het
 {
     public partial class Form1 : Form
     {
+        List<Flat> flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            flats = context.Flats.ToList();
         }
     }
 }
