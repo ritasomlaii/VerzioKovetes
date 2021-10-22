18d8bbdc4e36d8c303bf93fdea29f2c1c329ea5d
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-10-22 10:44:49 +0200

    ORM

--- VaR_D0zbsj/VaR_D0zbsj/Form1.cs
+++ VaR_D0zbsj/VaR_D0zbsj/Form1.cs
@@ -12,9 +12,17 @@ namespace VaR_D0zbsj
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+
+
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            List<Tick> l = context.Ticks.ToList();
         }
     }
 }
