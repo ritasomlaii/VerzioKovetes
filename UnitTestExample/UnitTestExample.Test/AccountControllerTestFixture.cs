6270f79aa4091b1e66d111df3edc056424726d27
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-11-26 20:55:45 +0100

    NuGet csomagok

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace UnitTestExample.Test
 {
