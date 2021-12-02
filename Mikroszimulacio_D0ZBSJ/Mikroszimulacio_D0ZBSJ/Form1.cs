09ca3bb58731175b6f5e7fd0f972068d23ef3f9d
ritasomlaii <rita.somlai@stud.uni-corvinus.hu>
2021-12-02 10:43:20 +0100

    adatok betöltése population

--- Mikroszimulacio_D0ZBSJ/Mikroszimulacio_D0ZBSJ/Form1.cs
+++ Mikroszimulacio_D0ZBSJ/Mikroszimulacio_D0ZBSJ/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,30 @@ namespace Mikroszimulacio_D0ZBSJ
         public Form1()
         {
             InitializeComponent();
+
+
+            Population = GetPopulation(@"C:\Temp\név.csv");
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
         }
     }
 }
